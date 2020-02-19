5d768d916a31d6e400653a3548b78d61511af26d
Muqsit <hackhack05@gmail.com>
2020-02-19 09:51:10 +0000

    Disable plugin if invalid API key provided, debug client data or else.

--- src/cosmicpe/novpn/Main.php
+++ src/cosmicpe/novpn/Main.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace cosmicpe\novpn;
 
 use cosmoverse\antivpn\AntiVPN;
+use cosmoverse\antivpn\api\client\AntiVPNClientResult;
+use cosmoverse\antivpn\thread\AntiVPNException;
 use pocketmine\plugin\PluginBase;
 
 final class Main extends PluginBase{
@@ -14,7 +16,21 @@ final class Main extends PluginBase{
 
 	public function onEnable() : void{
 		$this->api = new AntiVPN($this, (string) $this->getConfig()->get("api-key"), (int) $this->getConfig()->get("thread-count"));
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		$this->api->getClientData(
+			function(AntiVPNClientResult $result) : void{
+				$this->getLogger()->debug("Connected as " . $result);
+			},
+			function(AntiVPNException $exception) : void{
+				$this->getLogger()->logException($exception);
+				$this->getServer()->getPluginManager()->disablePlugin($this);
+			}
+		);
+
+		$this->api->waitAll();
+
+		if(!$this->isDisabled()){
+			$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		}
 	}
 
 	public function getApi() : AntiVPN{
